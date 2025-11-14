5a53ebd99d21baf1fb51b4b33d14bf1eeeab05c4
Peninah98 <peninangizwenayo@gmail.com>
2025-11-14 15:40:34 +0200

    feat: create base and login test classes

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,43 @@
+package base;
+
+import org.example.HomePage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.TestNG;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseTests {
+    private WebDriver  driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        TestNG testNG = new TestNG();
+
+        // Add test classes
+        List<Class> testClasses = new ArrayList<>();
+        testClasses.add(login.LoginTests.class);
+        testNG.setTestClasses(testClasses.toArray(new Class[0]));
+
+        // Run tests
+        testNG.run();
+    }
+}
