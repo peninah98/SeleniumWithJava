cc445aae9d2b44d8a8e048e5a571029df76009ac
Peninah98 <peninangizwenayo@gmail.com>
2025-12-02 18:56:26 +0200

    refactor: update the test base

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,18 +1,25 @@
 package base;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.google.common.io.Files;
 import org.example.HomePage;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.testng.ITestResult;
 import org.testng.TestNG;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import utils.WindowManager;
@@ -25,6 +32,7 @@ public class BaseTests {
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
     }
+
 
     public static void main(String[] args) {
         TestNG testNG = new TestNG();
@@ -45,15 +53,28 @@ public class BaseTests {
         goHome();
         homePage = new HomePage(driver);
     }
-    public WindowManager  getWindowManager(){
-        return new WindowManager(driver);
-    }
+
 
     @AfterClass
     public void tearDown() {
         driver.quit();
     }
+    @AfterMethod
+    public WindowManager  getWindowManager(){
+        return new WindowManager(driver);
+    }
+    public void recorFailure(ITestResult result) {
+        if(ITestResult.FAILURE == result.getStatus()){
+        var camera = (TakesScreenshot)driver;
+        File screenShot = camera.getScreenshotAs(OutputType.FILE);
 
+try {
+    Files.move(screenShot, new File("resources/screenshots/test.png"));
 
-
+} catch (IOException e) {
+    e.printStackTrace();
 }
+    }
+
+
+}}
